4d66d390d03a85523455f8f5f14391b00459b27c
sh414lim <77113306+sh414lim@users.noreply.github.com>
2023-01-26 17:16:11 +0900

    updste : 유저 생성 추가

--- unber_backend/src/users/user.resolver.ts
+++ unber_backend/src/users/user.resolver.ts
@@ -1,7 +1,11 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Resolver } from '@nestjs/graphql';
 import { User } from './entitis/user.entity';
 import { UsersService } from './users.service';
 import { Query } from '@nestjs/graphql';
+import {
+  CreateAccountInput,
+  CreateAccountOutput,
+} from './dtos/create-account.dto';
 
 @Resolver((of) => User)
 export class UserResolver {
@@ -10,5 +14,28 @@ export class UserResolver {
   @Query((resturns) => Boolean)
   hi() {
     return true;
+  }
+
+  @Mutation((returns) => CreateAccountOutput)
+  async createAccount(
+    @Args('input') createAccountInput: CreateAccountInput,
+  ): Promise<CreateAccountOutput> {
+    try {
+      const error = await this.userService.createAccount(createAccountInput);
+      if (error) {
+        return {
+          ok: false,
+          error,
+        };
+      }
+      return {
+        ok: true,
+      };
+    } catch (e) {
+      return {
+        error: e,
+        ok: false,
+      };
+    }
   }
 }
--- unber_backend/src/users/users.service.ts
+++ unber_backend/src/users/users.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { CreateAccountInput } from './dtos/create-account.dto';
 import { User } from './entitis/user.entity';
 
 @Injectable()
@@ -8,4 +9,25 @@ export class UsersService {
   constructor(
     @InjectRepository(User) private readonly users: Repository<User>,
   ) {}
+
+  async createAccount({
+    email,
+    password,
+    role,
+  }: CreateAccountInput): Promise<string | undefined> {
+    // check email  , new user
+    // create user pwd
+    try {
+      const exists = await this.users.findOne({ where: { email } });
+      if (exists) {
+        // make error
+        return '해당 이메일이 존재합니다';
+      }
+      await this.users.save(this.users.create({ email, password, role }));
+    } catch (e) {
+      //make error
+      console.log(e);
+      return '계정을 생성할 수 없습니다';
+    }
+  }
 }
