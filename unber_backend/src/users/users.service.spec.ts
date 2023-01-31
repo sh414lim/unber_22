f527b855c98d389120aaf13b6fc7231ebd8abe06
sh414lim <77113306+sh414lim@users.noreply.github.com>
2023-01-31 11:08:07 +0900

    update : mockup test module 생성

--- unber_backend/src/users/users.service.spec.ts
+++ unber_backend/src/users/users.service.spec.ts
@@ -1,12 +1,50 @@
 import { Test } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { JwtService } from 'src/jwt/jwt.service';
+import { MailService } from 'src/mail/mail.service';
+import { User } from './entitis/user.entity';
+import { Verification } from './entitis/verification.entity';
 import { UsersService } from './users.service';
+
+const mockRepositiory = {
+  findOne: jest.fn(),
+  save: jest.fn(),
+  create: jest.fn(),
+};
+
+const mockJwtService = {
+  sign: jest.fn(),
+  verify: jest.fn(),
+};
+
+const mockMailService = {
+  sendEmail: jest.fn(),
+};
 
 describe('UserService', () => {
   let service: UsersService;
 
   beforeAll(async () => {
     const module = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        {
+          provide: getRepositoryToken(User),
+          useValue: mockRepositiory,
+        },
+        {
+          provide: getRepositoryToken(Verification),
+          useValue: mockRepositiory,
+        },
+        {
+          provide: JwtService,
+          useValue: mockJwtService,
+        },
+        {
+          provide: MailService,
+          useValue: mockMailService,
+        },
+      ],
     }).compile();
     service = module.get<UsersService>(UsersService);
   });
